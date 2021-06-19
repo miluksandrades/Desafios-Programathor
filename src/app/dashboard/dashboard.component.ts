a5c7ebf1933e385bfbf44dca97d4bd12af460eef
Lucas Andrade <luksandrades18@gmail.com>
2021-06-19 12:01:44 -0300

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ export class DashboardComponent implements OnInit {
   public desafios = [
     {route: '/desafio-one', task: 'Desafio 1', description: 'Um aplicativo que registra peso', url: 'assets/desafio_1.png'},
     {route: '/desafio-two', task: 'Desafio 2', description: 'Um banco de dados de livros', url: 'assets/desafio_2.png'},
-    {route: '/desafio-three', task: 'Desafio 3', description: 'Um aplicativo de receitas', url: ''},
+    {route: '/desafio-three', task: 'Desafio 3', description: 'Um aplicativo de receitas', url: 'assets/desafio_3.png'},
     {route: '/desafio-four', task: 'Desafio 4', description: 'Um registrador de contas', url: ''},
     {route: '/desafio-five', task: 'Desafio 5', description: 'Um registrador de despesas', url: ''},
     {route: '/desafio-six', task: 'Desafio 6', description: 'Um aplicativo de chat', url: ''},
